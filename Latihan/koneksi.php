b2cfb97b09b0e76e2d3020862cddbbd1c898099f
Muhammad Dafha <134750606+Dafha22@users.noreply.github.com>
2023-08-16 16:15:19 +0700

    Add files via upload

---
+++ Latihan/koneksi.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+        file ini terbuat dari umbi umbian yang 
+        melakukan koneksi database, dengan menggunakan 
+        konser OOP Prosddural.
+
+        mysql_connect(serverAddr, user, pwd, db)
+    */
+
+    $hostname = "localhost" ;
+    $username = "root" ;
+    $password = "" ;
+    $database = "vokasi" ;
+
+
+    error_reporting(0);
+    mysqli_report(MYSQLI_REPORT_OFF);
+    
+    $conn = mysqli_connect($hostname,$username,$password,$database);
+
+    if (mysqli_connect_errno()){
+        die ("Ada Erro Coy... nih Disini : ".mysqli_connect_error());
+    }
+
+    // echo "DASHBOARD"
+?>
